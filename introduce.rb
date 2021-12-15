575272a0055f86964c3ec3f1480e4e9f35e2118d
taro.kanda <ko0814chata@gmail.com>
2021-12-15 11:33:18 +0000

    kadai-saiteisyutsu1

--- introduce.rb
+++ introduce.rb
@@ -1,5 +1,4 @@
 first="昂"
 family="茶谷"
 age= 30
-intro=family+first+"です。"+age.to_s+"歳です。"
-puts intro
+puts family+first+"です。"+age.to_s+"歳です。"
---
+++ kadai-introduce.rb
@@ -0,0 +1,4 @@
+first="昂"
+family="茶谷"
+age= 30
+puts family+first+"です。"+age.to_s+"歳です。"
